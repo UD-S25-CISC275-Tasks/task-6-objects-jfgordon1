b0abdb7a350d755de69791bcf4fdc9a1b0551c1b
jfgordon <jfgordon@udel.edu>
2025-02-26 10:59:21 -0500

    toShortForm and toMarkDown are both complete and passed testing

--- src/objects.ts
+++ src/objects.ts
@@ -65,7 +65,9 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const idName = question.id.toString();
+    const first10 = question.name.substring(0, 10);
+    return idName + ": " + first10;
 }
 
 /**
@@ -86,7 +88,18 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "multiple_choice_question") {
+        const markDownOptions = question.options.map(
+            (option: string): string => "- " + option,
+        );
+        const optionsString = markDownOptions.join("\n");
+        const questionName = "# " + question.name + "\n";
+        const questionBody = question.body + "\n";
+        return questionName + questionBody + optionsString;
+    }
+    const questionName = "# " + question.name + "\n";
+    const questionBody = question.body;
+    return questionName + questionBody;
 }
 
 /**
